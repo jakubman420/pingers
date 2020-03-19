f3222661ed0efd0f54e83a75af3791cfdc432025
Jakub Pingerman <62389034+jakubman420@users.noreply.github.com>
2020-03-19 14:25:18 +0000

    done man so good

---
+++ pinger.py
@@ -0,0 +1,22 @@
+"""
+written by jakub pingerman
+19-03-2020
+"""
+
+pingers=0
+max_pingers = 50
+
+while pingers != 50:
+    inp = input("are u ready to take another pinger?: ")
+
+    if inp == "yes":
+        print("ok man")
+        pingers += 1
+    elif inp == "no":
+        print("pussy")
+    else:
+        print("what the hell man that wasnt very cash money of u")
+    
+
+print("ok man that is enough pingers for today")
+
